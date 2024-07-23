a25e27418f28f4663e9e1772ce36b1a757f4ab5e
boxcolli <dmg04158@gmail.com>
2024-07-23 12:50:44 +0900

    defer works

--- app/routes/defer/loader.ts
+++ app/routes/defer/loader.ts
@@ -1,13 +1,2 @@
 import { defer, LoaderFunctionArgs } from "@remix-run/cloudflare";
 
-export async function $loader({ request, context: { env } }: LoaderFunctionArgs) {
-    const delay = 3
-    return defer({
-        delay,
-        hello: new Promise<string>((resolve) => {
-            setTimeout(() => {
-                resolve("Hello, World!")
-            }, delay * 1000)
-        }).catch((e) => { throw new Response("Oops", { status: 500 }) })
-    })
-}
--- app/routes/defer/route.tsx
+++ app/routes/defer/route.tsx
@@ -1,9 +1,21 @@
 import { Text } from "~/components/ui";
-import { $loader } from "./loader";
 import { Await, useLoaderData } from "@remix-run/react";
 import { Suspense } from "react";
+import { defer, LoaderFunctionArgs } from "@remix-run/cloudflare";
 
-export const loader = $loader
+export async function loader({ request, context: { env } }: LoaderFunctionArgs) {
+    const delay = 3
+    
+    return defer({
+        delay,
+        hello: new Promise<string>((resolve) => {
+            setTimeout(() => {
+                resolve("Hello, World!")
+            }, delay * 1000)
+        }).catch((e) => { throw new Response("Oops", { status: 500 }) })
+    })
+}
+
 
 export default function Index() {
     const { delay, hello } = useLoaderData<typeof loader>()
